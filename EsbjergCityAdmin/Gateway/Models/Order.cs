f065762decccb7a54b943a353fa66bd90775d990
Raakost <jeanettesydboege@gmail.com>
2016-12-06 10:04:18 +0100

    facade, gateways, models

---
+++ EsbjergCityAdmin/Gateway/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gateway.Models
+{
+    public class Order : AbstractId
+    {
+        public DateTime DateOfPurchase { get; set; }
+        public List<GiftCard> GiftCards { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
